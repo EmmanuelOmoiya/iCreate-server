30bb0234d86c2dff117bf76e857aa7a6e6d2f0e9
Emmanuel Omoiya <79797604+EmmanuelOmoiya@users.noreply.github.com>
2022-05-31 14:13:53 +0100

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs')
 
 const userModel = mongoose.Schema({
-    fullName: { type: String, required: true, unique},
+    fullName: { type: String, required: true},
     email: { type: String, unique: true , required: true},
     password: { type: String, required: true},
     address: { type: String },
